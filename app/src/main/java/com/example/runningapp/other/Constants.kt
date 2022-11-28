c921fbcb49415fb8acd9f9579e9e7e45ff969757
itsjustdave67 <itsjustdave67@gmail.com>
2022-11-28 11:15:55 +0300

    Setting up the Tracking Service 1. Configuration of the communication of activity to service 2. configuring type of intents communicated 3. evaluation of the service using logcat
    Setting up of the Foreground service
    1. creation of the notification channel
    2. initiation of the foreground service
    3. setup of the actual notification builder
    4. getting the pending intent from the main activity
    5. relocation to tracking fragment when notificiation is pressed
    6. setup of the onStartCommand
    
    Tracking of the User in the background
    1. configuration of the mutableLiveData to record the path taken by user
    2. setup of the location callbacks and updating the location tracking
    3. test of the movement using timber to display the latitude and longitude position.
    4. Setup of the route playback to be used for testing

--- app/src/main/java/com/example/runningapp/other/Constants.kt
+++ app/src/main/java/com/example/runningapp/other/Constants.kt
@@ -2,4 +2,19 @@ package com.example.runningapp.other
 
 object Constants {
     const val RUNNING_DATABASE_NAME = "running_db"
+
+    const val REQUEST_CODE_LOCATION_PERMISSIONS = 0
+
+    const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
+    const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
+    const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+
+    const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+    const val NOTIFICATION_ID =  1
+
+    const val LOCATION_UPDATE_INTERVAL = 5000L
+    const val FASTEST_LOCATION_INTERVAL = 2000L
+
+    const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
 }
